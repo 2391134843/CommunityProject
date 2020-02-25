ac6061fbbd0819d5562bba75bf2a2195e852bb0c
2391134843 <2391134843@qq.com>
2020-02-25 14:50:31 +0800

    improve github configuration

--- src/main/java/life/interview/community/community/controller/AuthorizeConroller.java
+++ src/main/java/life/interview/community/community/controller/AuthorizeConroller.java
@@ -1,4 +1,41 @@
 package life.interview.community.community.controller;
 
+import life.interview.community.community.dto.AccessTokenDTO;
+import life.interview.community.community.dto.GithubUser;
+import life.interview.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeConroller {
+    /*自动加载实例*/
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+//        accessTokenDTO.setClient_secret("e7e90a308978e8568ceab2486385ace282d146f5");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+                            return "index";
+    }
+
+
 }
